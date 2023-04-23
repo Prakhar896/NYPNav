ce1fa56b822e0a72ef4d59ed46a590e15222a909
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2023-04-23 22:52:00 +0800

    Initial Commit

---
+++ NYPNav/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NYPNav
+//
+//  Created by Prakhar Trivedi on 23/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NYPNav/NYPNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYPNavApp.swift
+//  NYPNav
+//
+//  Created by Prakhar Trivedi on 23/4/23.
+//
+
+import SwiftUI
+
+@main
+struct NYPNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
